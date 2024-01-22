baddfaba2e087b58995a6841b753442fc3f623c0
DmitryAData <dmitrysavinmedia@yandex.ru>
2024-01-23 00:23:38 +0300

    added ros2 control

--- launch/launch_sim.launch.py
+++ launch/launch_sim.launch.py
@@ -27,10 +27,24 @@ def generate_launch_description():
                         arguments=['-topic', 'robot_description',
                                   '-entity', 'das_robot'],
                         output='screen')
+    
+    diff_drive_spawner = Node(
+        package="controller_manager",
+        executable="spawner.py",
+        arguments=["diff_cont"]
+    )
+
+    joint_broad_spawner = Node(
+        package="controller_manager",
+        executable="spawner.py",
+        arguments=["joint_broad"]
+    )
 
     # Запускаем
     return LaunchDescription([
         rsp,
         gazebo,
-        spawn_entity
+        spawn_entity,
+        diff_drive_spawner,
+        joint_broad_spawner
     ])

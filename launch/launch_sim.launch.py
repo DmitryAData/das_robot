4af469281239a1b923d243c541c0809085f330c4
DmitryAData <100353146+DmitryAData@users.noreply.github.com>
2024-01-22 21:52:51 +0300

    Add files via upload

---
+++ launch/launch_sim.launch.py
@@ -0,0 +1,36 @@
+# Необходимые для работы библиотеки
+import os
+from ament_index_python.packages import get_package_share_directory
+from launch import LaunchDescription
+from launch.actions import IncludeLaunchDescription
+from launch.launch_description_sources import PythonLaunchDescriptionSource
+from launch_ros.actions import Node
+
+# Функция генерирующая описание запуска
+def generate_launch_description():
+
+    # Сохраняем имя пакета
+    package_name='das_robot'
+
+    # Поолучаем описание робота из лаунч файла
+    rsp = IncludeLaunchDescription(
+        PythonLaunchDescriptionSource([os.path.join(
+            get_package_share_directory(package_name),'launch','rsp.launch.py'
+            )]), launch_arguments={'use_sim_time': 'true'}.items())
+
+    # Получаем файлы запуска Gazebo
+    gazebo = IncludeLaunchDescription(PythonLaunchDescriptionSource([os.path.join(
+            get_package_share_directory('gazebo_ros'), 'launch', 'gazebo.launch.py')]))
+
+    # Создаем робота в симуляции
+    spawn_entity = Node(package='gazebo_ros', executable='spawn_entity.py',
+                        arguments=['-topic', 'robot_description',
+                                  '-entity', 'das_robot'],
+                        output='screen')
+
+    # Запускаем
+    return LaunchDescription([
+        rsp,
+        gazebo,
+        spawn_entity
+    ])
---
+++ launch/rsp.launch.py
@@ -0,0 +1,26 @@
+# Необходимые для работы библиотеки
+import os
+from ament_index_python.packages import get_package_share_directory
+from launch import LaunchDescription
+from launch.substitutions import LaunchConfiguration
+from launch.actions import DeclareLaunchArgument
+from launch_ros.actions import Node
+import xacro
+
+# Функция генерирующая описание запуска
+def generate_launch_description():
+
+    # Переменная для хранения запроса на хранение синхронизации по времени
+    use_sim_time = LaunchConfiguration('use_sim_time')
+
+    # Получаем описание робота
+    pkg_path = os.path.join(get_package_share_directory('das_robot')) # Ищем пакет
+    xacro_file = os.path.join(pkg_path,'description','robot.urdf.xacro') # Указывваем на URDF файл в пакете
+    robot_description_config = xacro.process_file(xacro_file).toxml() # Помещаем обработанное с помощью xacro описание робота в файл
+
+    # Создаем ноду для joint_state_publisher
+    params = {'robot_description': robot_description_config, 'use_sim_time': use_sim_time} # Берем параметры из переменных
+    node_robot_state_publisher = Node(package='robot_state_publisher', executable='robot_state_publisher', output='screen', parameters=[params]) # Создаем ноду
+
+    # Запускаем
+    return LaunchDescription([DeclareLaunchArgument('use_sim_time', default_value='false', description='Use sim time if true'), node_robot_state_publisher])
